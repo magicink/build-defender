7af03a5062a719707887cbcbb9f6a53f96a8c7a6
Brandon Tom <magicink@gmail.com>
2021-01-03 09:31:59 -0800

    created outline shader

---
+++ Assets/Scripts/Sprites/SpriteSortOrderController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSortOrderController : MonoBehaviour
+{
+    [SerializeField] private bool runOnce;
+    [SerializeField] private Transform root;
+    [SerializeField] private float sortPrecision = 5.0f;
+    [SerializeField] private List<SpriteRenderer> sprites;
+
+    private void Start()
+    {
+        root = transform.parent;
+    }
+
+    private void LateUpdate()
+    {
+        if (root)
+        {
+            var next = 0.0f;
+            for (var i = sprites.Count - 1; i >= 0; i--)
+            {
+                var rootPosition = root.transform.position;
+                var baseSort = -(rootPosition.y * sortPrecision) - next;
+                next++;
+                sprites[i].sortingOrder = (int) baseSort;
+            }
+        }
+        if (runOnce) Destroy(this);
+    }
+}
