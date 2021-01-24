2e30ca0f6c7efed6c924ac96d52c0fd4711d564c
Brandon Tom <magicink@gmail.com>
2021-01-24 09:44:19 -0800

    added placement restrictions

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -30,11 +30,22 @@ public class BuildingManager : MonoBehaviour
 
     private void Update()
     {
+        var mousePosition = Utils.GetMousePosition();
         if (!_currentBuilding || !Input.GetMouseButtonDown(0) || EventSystem.current.IsPointerOverGameObject()) return;
+        if (!CanSpawn(_currentBuilding, mousePosition)) return;
         var buildingPrefab = _currentBuilding.prefab;
         if (buildingPrefab)
         {
-            Instantiate(buildingPrefab, Utils.GetMousePosition(), Quaternion.identity);
+            Instantiate(buildingPrefab, mousePosition, Quaternion.identity);
         }
+    }
+
+    private static bool CanSpawn(BuildingType buildingType, Vector3 position)
+    {
+        var collider2d = buildingType.prefab.GetComponent<BoxCollider2D>();
+        if (!collider2d) return false;
+        var collisions = Physics2D.OverlapBoxAll(position + (Vector3) collider2d.offset, collider2d.size, 0);
+        // 2 Because the ghost building has a collider
+        return collisions.Length < 2;
     }
 }
