2ea73d6d909b5088ae559cb5abc4b8fa14157ec2
Brandon Tom <magicink@gmail.com>
2021-02-07 08:33:28 -0800

    clean up

--- Assets/ResourceGeneratorUI.cs
+++ Assets/ResourceGeneratorUI.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,18 +5,12 @@ public class ResourceGeneratorUI : MonoBehaviour
 {
     [SerializeField] private BuildingData buildingData;
     [SerializeField] private Image resourceIcon;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
         if (buildingData && resourceIcon)
         {
             resourceIcon.sprite = buildingData.BuildingType.resourceGeneratorData.ResourceType.icon;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
