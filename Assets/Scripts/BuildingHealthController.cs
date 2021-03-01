0117e82dd31a00d49371ef3a0f4c8c9b59970a50
Brandon Tom <magicink@gmail.com>
2021-03-01 07:09:37 -0800

    changed the camera shake intensity

--- Assets/Scripts/BuildingHealthController.cs
+++ Assets/Scripts/BuildingHealthController.cs
@@ -3,7 +3,7 @@
     public override void OnHealthChange(int next, int current)
     {
         if (next >= current) return;
-        var intensity = next <= 0 ? 10f : 5f;
+        var intensity = next <= 0 ? 5f : 2f;
         CameraShake.Instance.StartShake(intensity);
     }
 }
